f130aa558e049f77e32c7b362cb5828a2632fd36
Joyce Lynn <joycelyuz@gmail.com>
2024-12-12 20:26:30 -0800

    make initials for profile pic

--- client/components/HomeContent.tsx
+++ client/components/HomeContent.tsx
@@ -13,20 +13,26 @@ const QUERY = graphql`
       ...PostBox_user
     }
   }
+  genAllPosts {
+    content
+    genAuthor {
+      firstName
+      lastName
+    }
+  }
 `;
 
 export default function HomeContent(): JSX.Element {
-  const{username, password} = useContext(AuthContext);
+  const { username, password } = useContext(AuthContext);
   const data = useLazyLoadQuery<HomeContentQuery>(QUERY, {
     username,
     password,
-
   });
   const user = data.genUser;
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-gradient-to-br from-blue-200 via-blue-100 to-blue-300 text-gray-800">
       {" "}
-      {user != null && <PostBox user ={user}/>}
+      {user != null && <PostBox user={user} />}
     </div>
   );
 }
